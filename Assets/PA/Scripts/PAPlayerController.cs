92fe0e4d98e04d9396bd56471c8fdcc6efc05107
Orlando Parisblue <orlando.parisblue@ailo.io>
2025-01-25 09:54:02 +1100

    Add input manager

--- Assets/PA/Scripts/PAPlayerController.cs
+++ Assets/PA/Scripts/PAPlayerController.cs
@@ -3,33 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PAPlayerController : MonoBehaviour
-{
+public class PAPlayerController : MonoBehaviour {
   public float MovementSpeed = 1.0f;
   [SerializeField]
   private Rigidbody2D RB2D;
   private SpriteRenderer _SR;
 
   private Vector2 movement = Vector2.zero;
-    // Start is called before the first frame update
-    void Start()
-    {
+  // Start is called before the first frame update
+  void Start() {
     if (RB2D == null)
-      RB2D = this.gameObject.GetComponent<Rigidbody2D> ();
+      RB2D = this.gameObject.GetComponent<Rigidbody2D>();
     if (_SR == null)
-      _SR = GetComponent<SpriteRenderer> ();
-
-    }
-
-  public void Move (InputAction.CallbackContext context) {
-    movement = context.ReadValue<Vector2> () * MovementSpeed;
-    _SR.flipX = Vector2.Dot ( movement,Vector2.right) < 0;
+      _SR = GetComponent<SpriteRenderer>();
 
   }
 
-  void FixedUpdate ()
-  {
-    RB2D.AddForce ( (movement - RB2D.velocity) * Time.deltaTime, ForceMode2D.Impulse);
+  public void Move(InputAction.CallbackContext context) {
+    movement = context.ReadValue<Vector2>() * MovementSpeed;
+    _SR.flipX = Vector2.Dot(movement, Vector2.right) < 0;
+
+  }
+
+  void FixedUpdate() {
+    RB2D.AddForce((movement - RB2D.velocity) * Time.deltaTime, ForceMode2D.Impulse);
     // 'Move' code here
   }
 }
