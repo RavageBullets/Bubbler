e7e81faae696e8d1966a0a41218d7ab9c314a27c
Orlando Parisblue <orlando.parisblue@ailo.io>
2025-01-25 15:01:34 +1100

    Clean up / fix hats

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,60 +4,56 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
   public static GameManager instance;
   [SerializeField]
   private List<GameObject> PlayerList;
 
-  private void Awake ()
-  {
+  private void Awake() {
     if (instance == null) {
       instance = this;
     } else {
-      Destroy (gameObject);
+      Destroy(gameObject);
     }
   }
 
-  public void OnPlayerJoined (PlayerInput playerInput) {
-    Debug.Log ("Player Joined Yay");
-    AddPlayer (playerInput.gameObject);
+  public void OnPlayerJoined(PlayerInput playerInput) {
+    Debug.Log("Player Joined Yay");
+    AddPlayer(playerInput.gameObject);
   }
 
-  public void AddPlayer (GameObject playerObject)
-  {
+  public void AddPlayer(GameObject playerObject) {
     if (PlayerList == null) {
-      PlayerList = new List<GameObject> ();
+      PlayerList = new List<GameObject>();
     }
-    PlayerList.Add (playerObject);
+    PlayerList.Add(playerObject);
 
-    playerObject.GetComponent<PAPlayerController>().SetHat( this.gameObject.GetComponent<PlayerColourIndicators>().GetNextHat () );
-    Debug.Log ("Player " + playerObject.name + " Added");
+    playerObject.GetComponent<PlayerController>().SetHat(this.gameObject.GetComponent<PlayerColourIndicators>().GetNextHat());
+    Debug.Log("Player " + playerObject.name + " Added");
   }
 
 
-  public void RemovePlayer (GameObject playerObject) {
-    PlayerList.Remove (playerObject);
+  public void RemovePlayer(GameObject playerObject) {
+    PlayerList.Remove(playerObject);
 
     if (PlayerList.Count <= 1) {
-      EndOfRound ();
+      EndOfRound();
     }
   }
 
-  public void EndOfRound () {
-    Debug.Log ("Player " + PlayerList [0].name + " Wins!!!");
-    StartCoroutine ("RestartLevel");
+  public void EndOfRound() {
+    Debug.Log("Player " + PlayerList[0].name + " Wins!!!");
+    StartCoroutine("RestartLevel");
   }
 
 
-  private IEnumerator RestartLevel () {
-    yield return new WaitForSeconds (3); 
-    SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+  private IEnumerator RestartLevel() {
+    yield return new WaitForSeconds(3);
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
   }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update() {
+
+  }
 }
--- Assets/Scripts/PickHat.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PickHat : MonoBehaviour {
-  public SpriteRenderer spriteRenderer;
-
-  void Start() {
-
-  }
-}
