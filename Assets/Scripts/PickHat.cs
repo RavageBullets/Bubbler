108d258a14abe8989ac962f189f1882ca9a1a250
Peter Armstrong <ashen_s@yahoo.com.au>
2025-01-25 12:46:36 +1100

    Start of a GameManager.
    Consolidates some off the code to work with Unity's PlayerInputManager and the GameManager instead of off in its own little PickHat script

--- Assets/Scripts/PickHat.cs
+++ Assets/Scripts/PickHat.cs
@@ -4,10 +4,6 @@ public class PickHat : MonoBehaviour {
   public SpriteRenderer spriteRenderer;
 
   void Start() {
-    GameObject playerInput = GameObject.Find("Player Input");
-    PlayerColourIndicators indicators = playerInput.GetComponent<PlayerColourIndicators>();
-    if (indicators == null) return;
 
-    spriteRenderer.sprite = indicators.GetNextHat();
   }
 }
