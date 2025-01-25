51aaec15a2d71847e27f9b99c6db86c80817c2cd
Orlando Parisblue <orlando.parisblue@ailo.io>
2025-01-25 17:56:52 +1100

    Portals

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+  public Teleport target;
+  public float timeToDisableAfterExitSeconds = 1f;
+
+  private bool disabled = false;
+  private float disabledTime = 0;
+
+  void Update() {
+    if (disabled && Time.fixedTime - disabledTime >= timeToDisableAfterExitSeconds) {
+      disabled = false;
+    }
+  }
+
+  void OnTriggerEnter2D(Collider2D collider) {
+    if (disabled) return;
+    target.TemporarilyDisable();
+    collider.transform.position = target.transform.Find("Exit").transform.position;
+  }
+
+  public void TemporarilyDisable() {
+    disabled = true;
+    disabledTime = Time.fixedTime;
+  }
+}
