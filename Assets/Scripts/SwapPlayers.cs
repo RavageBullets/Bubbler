8284e4d2c20ea944529d4abb098c14428fff959b
Orlando Parisblue <orlando.parisblue@ailo.io>
2025-01-25 20:56:37 +1100

    Swap button

--- Assets/Scripts/SwapPlayers.cs
+++ Assets/Scripts/SwapPlayers.cs
@@ -2,8 +2,38 @@ using UnityEngine;
 
 public class SwapPlayers : MonoBehaviour, IInteractable {
 
+  public ParticleSystem teleportationParticles;
+
   public void Interact(GameObject player) {
-    Debug.Log("Test!");
+    GameObject[] players = Shuffle(GameObject.FindGameObjectsWithTag("Player"));
+
+    for (int i = 0; i < players.Length; i += 2) {
+      if (i + 1 >= players.Length) break;
+
+      GameObject playerA = players[i];
+      GameObject playerB = players[i + 1];
+
+
+      Rigidbody2D rigidbodyA = playerA.GetComponent<Rigidbody2D>();
+      Rigidbody2D rigidbodyB = playerB.GetComponent<Rigidbody2D>();
+
+      (playerA.transform.position, playerB.transform.position) = (playerB.transform.position, playerA.transform.position);
+      (rigidbodyA.velocity, rigidbodyB.velocity) = (rigidbodyB.velocity, rigidbodyA.velocity);
+
+      Instantiate(teleportationParticles, playerA.transform.position, playerA.transform.rotation);
+      Instantiate(teleportationParticles, playerB.transform.position, playerB.transform.rotation);
+    }
   }
 
+  private T[] Shuffle<T>(T[] items) {
+    T[] result = (T[])items.Clone();
+
+    for (int i = 0; i < items.Length; i++) {
+      int forwardIndex = Random.Range(i, items.Length);
+      (items[i], items[forwardIndex]) = (items[forwardIndex], items[i]);
+    }
+
+    return result;
+  }
 }
+
