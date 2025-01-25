ae5888d3a7c09358ca0aac48c6f6f774f6d0f63a
Gabriel Merida <kafkiacode@gmail.com>
2025-01-25 15:13:09 +1100

    feat: move (and flip) weapon in the direction of aiming

--- Assets/Scripts/WeaponControls.cs
+++ Assets/Scripts/WeaponControls.cs
@@ -7,6 +7,15 @@ public class WeaponControls : MonoBehaviour {
     Vector2 lookXY = context.ReadValue<Vector2>();
     float lookDirection = Vector2.SignedAngle(Vector2.right, lookXY);
 
-    transform.rotation = Quaternion.Euler(0, 0, lookDirection);
+    transform.parent.rotation = Quaternion.Euler(0, 0, lookDirection);
+
+    // make child weapon look
+    SpriteRenderer childWeapon = GetComponentInChildren<SpriteRenderer>();
+    if(childWeapon) {
+      bool isLeft = lookXY.x < 0;
+      childWeapon.flipX = isLeft;
+      childWeapon.transform.localEulerAngles = isLeft ? new Vector3(0,0,180) : Vector3.zero;
+    }
+
   }
 }
