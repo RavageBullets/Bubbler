bf3424f378b19d742e3ecd4b0b0be0c026262359
Orlando Parisblue <orlando.parisblue@ailo.io>
2025-01-25 12:36:30 +1100

    Kill bubbles on collision

--- Assets/Scripts/DieAfterTime.cs
+++ Assets/Scripts/DieAfterTime.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 
-public class DieAfterTime : MonoBehaviour {
+public class DieAfterTimeOrCollision : MonoBehaviour {
+
 
   public float timeToLiveSeconds = 2f;
+
+  public bool dieAfterCollision = true;
+  public float invulnerablePeriodSeconds = 0.01f;
 
   private float timeOfInstantiation = 0;
 
@@ -12,6 +16,12 @@ public class DieAfterTime : MonoBehaviour {
 
   void Update() {
     if (Time.fixedTime - timeOfInstantiation > timeToLiveSeconds) {
+      Destroy(gameObject);
+    }
+  }
+
+  void OnCollisionEnter2D() {
+    if (dieAfterCollision && Time.fixedTime - timeOfInstantiation > invulnerablePeriodSeconds) {
       Destroy(gameObject);
     }
   }
