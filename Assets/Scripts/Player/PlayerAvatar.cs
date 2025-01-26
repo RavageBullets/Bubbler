228629a95ae4ab0b6b114b928737c7b032073fe1
CrumbleThorn <jctamayo5@alum.up.edu.ph>
2025-01-26 13:38:05 +1100

    Added rigged player character prefabs

---
+++ Assets/Scripts/Player/PlayerAvatar.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerAvatar : MonoBehaviour
+{
+    [SerializeField] public SpriteRenderer LeftArm;
+    [SerializeField] public SpriteRenderer RightArm;
+    [SerializeField] public Transform LeftAnchor;
+    [SerializeField] public Transform RightAnchor;
+}
