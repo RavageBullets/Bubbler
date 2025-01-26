6753c951a106f40e6e36a0ee773d033d7008172b
Orlando Parisblue <orlando.parisblue@ailo.io>
2025-01-26 15:10:06 +1100

    Show avatar on round over screen

--- Assets/Scripts/Player/PlayerAvatar.cs
+++ Assets/Scripts/Player/PlayerAvatar.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 using UnityEngine.U2D.IK;
 
-public class PlayerAvatar : MonoBehaviour
-{
-    [SerializeField] private LimbSolver2D leftArmIK;
-    [SerializeField] private LimbSolver2D rightArmIK;
-    public SpriteRenderer LeftArm;
-    public SpriteRenderer RightArm;
-    public Transform LeftAnchor;
-    public Transform RightAnchor;
+public class PlayerAvatar : MonoBehaviour {
+  [SerializeField] private LimbSolver2D leftArmIK;
+  [SerializeField] private LimbSolver2D rightArmIK;
+  public SpriteRenderer LeftArm;
+  public SpriteRenderer RightArm;
+  public Transform LeftAnchor;
+  public Transform RightAnchor;
 
-    public void UpdateInverseKinematics (Transform gunAnchorL, Transform gunAnchorR) {
-        leftArmIK.GetChain(2).target = gunAnchorL;
-        rightArmIK.GetChain(2).target = gunAnchorR;
-    }
+  public Sprite staticSprite;
+
+  public void UpdateInverseKinematics(Transform gunAnchorL, Transform gunAnchorR) {
+    leftArmIK.GetChain(2).target = gunAnchorL;
+    rightArmIK.GetChain(2).target = gunAnchorR;
+  }
 }
 
