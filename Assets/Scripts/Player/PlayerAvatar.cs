e16cca616e9609ad1f851f5c939aebe764562299
CrumbleThorn <jctamayo5@alum.up.edu.ph>
2025-01-26 14:44:36 +1100

    Added dynamic IK assignment

--- Assets/Scripts/Player/PlayerAvatar.cs
+++ Assets/Scripts/Player/PlayerAvatar.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.U2D.IK;
 
 public class PlayerAvatar : MonoBehaviour
 {
-    [SerializeField] public SpriteRenderer LeftArm;
-    [SerializeField] public SpriteRenderer RightArm;
-    [SerializeField] public Transform LeftAnchor;
-    [SerializeField] public Transform RightAnchor;
+    [SerializeField] private LimbSolver2D leftArmIK;
+    [SerializeField] private LimbSolver2D rightArmIK;
+    public SpriteRenderer LeftArm;
+    public SpriteRenderer RightArm;
+    public Transform LeftAnchor;
+    public Transform RightAnchor;
+
+    public void UpdateInverseKinematics (Transform gunAnchorL, Transform gunAnchorR) {
+        leftArmIK.GetChain(2).target = gunAnchorL;
+        rightArmIK.GetChain(2).target = gunAnchorR;
+    }
 }
+
