245fe5b7b6a500955b2af0f34a18dffdc2460bb0
Gabriel Merida <kafkiacode@gmail.com>
2025-01-26 14:22:04 +1100

    feat: music for Victory (end round)

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -32,17 +32,28 @@ public class MusicManager : MonoBehaviour {
     private void ChangedActiveScene(Scene current, Scene next) {
         string currentName = current.name;
         string nextName = next.name;
+        AudioSource _as = Instance.GetComponent<AudioSource>();
         if (nextName.Substring(0, 5) == "Level" || nextName.Substring(0, 5) == "Arena"
         ) {
-            this.GetComponent<AudioSource>().clip = battleClip;
-            this.GetComponent<AudioSource>().Play();
+            _as.loop = true;
+            _as.clip = battleClip;
+            _as.Play();
         } else if (nextName == "Main Menu") {
-            this.GetComponent<AudioSource>().clip = this.prepostClip;
-            this.GetComponent<AudioSource>().Play();
+            _as.loop = true;
+            _as.clip = this.prepostClip;
+            _as.Play();
         } else if (nextName == "Join Menu") {
-            this.GetComponent<AudioSource>().clip = this.prepostClip;
-            this.GetComponent<AudioSource>().Play();
+            _as.loop = true;
+            _as.clip = this.prepostClip;
+            _as.Play();
         }
+    }
+
+    public static void PlayVictory() {
+        AudioSource _as = Instance.GetComponent<AudioSource>();
+        _as.loop = false;
+        _as.clip = Instance.defeatClip;
+        _as.Play();
     }
 
 }
